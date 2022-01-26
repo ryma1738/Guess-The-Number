ba344ccae88f1a70631c59cd61eef2a1ccb16bcc
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-01-26 14:06:58 -0700

    completed project 4 requirements

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,19 +1,22 @@
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void guessRight() {
+        assertEquals("true", App.guess(6, 6));
+    }
+
+    @Test
+    public void guessHigh() {
+        assertEquals("You guess is too high.", App.guess(5, 20));
+    }
+
+    @Test
+    public void guessLow() {
+        assertEquals("You guess is too low.", App.guess(20, 5));
     }
 }
